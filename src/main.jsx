e2d8ebeba46305a4022da67b44e76a881d3b7c35
Eduardo Barroso <edubarrosoparra@gmail.com>
2025-11-09 21:34:13 +0100

    cambis

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="./">
+    <BrowserRouter basename="/entrega-ejercicios/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/entrega-ejercicios/", //nombre exacto del repositorio
   build: {
-    outDir:"docs",
-  },
-  server: {
-    historyApiFallback: true,
+    outDir: "docs",
   },
 });
